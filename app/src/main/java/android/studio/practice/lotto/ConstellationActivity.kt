2041963c7a155d723a9c76f32e6fbf5da37b7735
hanbinseo <shb204@gmail.com>
2021-05-11 12:39:36 +0900

    initial commit

---
+++ app/src/main/java/android/studio/practice/lotto/ConstellationActivity.kt
@@ -0,0 +1,11 @@
+package android.studio.practice.lotto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstellationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constellation)
+    }
+}
