4d880e78b542b609199c850ce277e4a2835601d5
hanbinseo <shb204@gmail.com>
2021-05-26 13:24:54 +0900

    Random Number Generation

--- app/src/main/java/android/studio/practice/lotto/MainActivity.kt
+++ app/src/main/java/android/studio/practice/lotto/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.cardview.widget.CardView
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +17,9 @@ class MainActivity : AppCompatActivity() {
         val NameCardView = findViewById<CardView>(R.id.NameCardView)
 
         RandomCardView.setOnClickListener {
-            startActivity(Intent(this@MainActivity, ResultActivity::class.java))
+            val intent = Intent(this@MainActivity, ResultActivity::class.java)
+            intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers()))
+            startActivity(intent)
         }
         ConstellCardView.setOnClickListener {
             startActivity(Intent(this@MainActivity, ConstellationActivity::class.java))
@@ -23,5 +27,31 @@ class MainActivity : AppCompatActivity() {
         NameCardView.setOnClickListener {
             startActivity(Intent(this@MainActivity, NameActivity::class.java))
         }
+    }
+
+    fun getRandomLottoNumber():Int{
+        return Random().nextInt(45)+1
+    }
+
+    fun getRandomLottoNumbers():MutableList<Int>{
+        val lottoNumbers = mutableListOf<Int>()
+
+        while (true){
+            var number : Int = getRandomLottoNumber()
+            var flag_existing : Int = 0
+            for(j in 0..lottoNumbers.size){
+                if(lottoNumbers.contains(number)){
+                    flag_existing = 1
+                    break
+                }
+            }
+            if (flag_existing.equals(1))
+                continue
+            else
+        lottoNumbers.add(number)
+        if(lottoNumbers.size >= 6)
+                break
+        }
+        return lottoNumbers
     }
 }
