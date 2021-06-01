d618f83d1ba850db2a70d13974c41f03bb650392
hanbinseo <shb204@gmail.com>
2021-06-01 10:43:40 +0900

    Hash code generation based on DatePicker

--- app/src/main/java/android/studio/practice/lotto/ConstellationActivity.kt
+++ app/src/main/java/android/studio/practice/lotto/ConstellationActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CalendarView
 import android.widget.DatePicker
 import android.widget.TextView
 import org.w3c.dom.Text
@@ -19,6 +20,21 @@ class ConstellationActivity : AppCompatActivity() {
 
         val datePicker = findViewById<DatePicker>(R.id.datePicker)
         val txtConstell = findViewById<TextView>(R.id.txtConstell)
+        val calendar = Calendar.getInstance()
+
+        datePicker.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH),
+                object : CalendarView.OnDateChangeListener, DatePicker.OnDateChangedListener {
+                    override fun onDateChanged(view: DatePicker?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+                        txtConstell.text = makeConstellationString(datePicker.month, datePicker.dayOfMonth)
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onSelectedDayChange(p0: CalendarView, p1: Int, p2: Int, p3: Int) {
+                        TODO("Not yet implemented")
+                    }
+
+                }
+        )
 
         btnGoResultConstell.setOnClickListener {
             val intent = Intent(this, ResultActivity::class.java)
